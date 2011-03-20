d6950d05002abf63a0fce7d95e35a8f2c466dbf4
bleis-tift <bleis-tift@hotmail.co.jp>
2011-03-20 23:49:29 +0900

    ハッシュタグの判定をとりあえず実装 refs #2

--- MotsunabeZombieProject.Tests/TweetCategorizerTest.cs
+++ MotsunabeZombieProject.Tests/TweetCategorizerTest.cs
@@ -17,5 +17,13 @@ namespace MotsunabeZombieProject.Tests
             var result = categorizer.Categorize("bleis\tほげほげ");
             Assert.That(result, Is.EqualTo("Normal\tほげほげ"));
         }
+
+        [Test]
+        public void ハッシュタグ付きのTweetがHashTagに判定される()
+        {
+            var categorizer = new TweetCategorizer();
+            var result = categorizer.Categorize("bleis\tほげほげ #hash");
+            Assert.That(result, Is.EqualTo("HashTag\tほげほげ #hash"));
+        }
     }
 }
--- MotsunabeZombieProject/TweetCategorizer.cs
+++ MotsunabeZombieProject/TweetCategorizer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MotsunabeZombieProject
 {
@@ -10,6 +11,9 @@ namespace MotsunabeZombieProject
         public string Categorize(string record)
         {
             var body = record.Split(new[] { '\t' }, 2);
+            var regex = new Regex("#[a-zA-Z0-9_]");
+            if (regex.IsMatch(body[1]))
+                return "HashTag\t" + body[1];
             return "Normal\t" + body[1];
         }
     }
