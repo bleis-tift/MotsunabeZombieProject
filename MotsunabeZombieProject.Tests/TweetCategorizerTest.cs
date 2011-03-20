da19397e4469d3495ead1c48d13ffb6ee2be1e59
bleis-tift <bleis-tift@hotmail.co.jp>
2011-03-20 23:38:47 +0900

    実装完了 refs #1

---
+++ MotsunabeZombieProject.Tests/TweetCategorizerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MotsunabeZombieProject;
+
+namespace MotsunabeZombieProject.Tests
+{
+    [TestFixture]
+    class TweetCategorizerTest
+    {
+        [Test]
+        public void 普通のTweetがNormalに判定される()
+        {
+            var categorizer = new TweetCategorizer();
+            var result = categorizer.Categorize("bleis\tほげほげ");
+            Assert.That(result, Is.EqualTo("Normal\tほげほげ"));
+        }
+    }
+}
---
+++ MotsunabeZombieProject/TweetCategorizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotsunabeZombieProject
+{
+    public class TweetCategorizer
+    {
+        public string Categorize(string record)
+        {
+            var body = record.Split(new[] { '\t' }, 2);
+            return "Normal\t" + body[1];
+        }
+    }
+}
