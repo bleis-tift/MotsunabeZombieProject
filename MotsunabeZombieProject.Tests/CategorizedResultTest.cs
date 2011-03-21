2514acd42a1bd9d0b63474f5ef676e12a4fa8b71
bleis-tift <bleis-tift@hotmail.co.jp>
2011-03-21 10:30:45 +0900

    CategorizedResultクラスの導入

---
+++ MotsunabeZombieProject.Tests/CategorizedResultTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MotsunabeZombieProject;
+
+namespace MotsunabeZombieProject.Tests
+{
+    [TestFixture]
+    class CategorizedResultTest
+    {
+        [Test]
+        public void 元のTweet本体を持っている()
+        {
+            var cr = new CategorizedResult("ほげほげ");
+            Assert.That(cr.Body, Is.EqualTo("ほげほげ"));
+        }
+
+        [Test]
+        public void 判別結果を持っている()
+        {
+            var cr = new CategorizedResult("ほげほげ", "Normal");
+            Assert.That(cr.Categories, Is.EqualTo(new[] { "Normal" }));
+        }
+
+        [TestCase("hogehoge", new[] { "Normal" }, "Normal\thogehoge")]
+        [TestCase("ほげほげ", new[] { "Reply", "HashTag" }, "Reply,HashTag\tほげほげ")]
+        public void 文字列に変換できる(string body, string[] categories, string expected)
+        {
+            var cr = new CategorizedResult(body, categories);
+            Assert.That(cr.ToString(), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ MotsunabeZombieProject/CategorizedResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace MotsunabeZombieProject
+{
+    public class CategorizedResult
+    {
+        public CategorizedResult(string body, params string[] categories)
+        {
+            Debug.Assert(categories != null && categories.Length > 0);
+            Body = body;
+            Categories = categories;
+        }
+
+        public string Body { get; private set; }
+
+        public IEnumerable<string> Categories { get; private set; }
+
+        public override string ToString()
+        {
+            return string.Join(",", Categories) + "\t" + Body;
+        }
+    }
+}
