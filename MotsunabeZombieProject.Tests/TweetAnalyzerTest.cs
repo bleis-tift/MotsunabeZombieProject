4a1cd2075ce627da25897e81b46a1a6a0089c758
bleis-tift <bleis-tift@hotmail.co.jp>
2011-03-21 12:13:24 +0900

    実装完了

---
+++ MotsunabeZombieProject.Tests/TweetAnalyzerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MotsunabeZombieProject;
+
+namespace MotsunabeZombieProject.Tests
+{
+    [TestFixture]
+    class TweetAnalyzerTest
+    {
+        [TestCaseSource("TweetsAndExpectedCategories")]
+        public void ネットワークからTweetを取得して解析できる(string[] tweets, string[][] expectedCategories)
+        {
+            var ta = new TweetAnalyzer()
+            {
+                TweetProvider = new TweetProvider(tweets)
+            };
+            Assert.That(ta.Categorize("http://192.168.1.40:4567/public_timeline").Select(r => r.Categories), Is.EqualTo(expectedCategories));
+        }
+
+        static object[][] TweetsAndExpectedCategories = new[] {
+            new object[] { new[] { "2011/03/21 12:16:25\tbleis\tほげほげ" }, new[] { new[] { "Normal" } } },
+            new object[] { new[] { "2011/03/21 12:16:25\tbleis\tほげほげ", "2011/03/21 12:22:30\tmzp\t@bleis ほげほげ" }, new[] { new[] { "Normal" }, new [] { "Reply" } } }
+        };
+    }
+}
---
+++ MotsunabeZombieProject/TweetAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace MotsunabeZombieProject
+{
+    public class TweetAnalyzer
+    {
+        public TweetAnalyzer()
+        {
+            TweetProvider = new TweetProvider(null);
+        }
+
+        internal IEnumerable<CategorizedResult> Categorize(string url)
+        {
+            var categorizer = new TweetCategorizer();
+            return TweetProvider.GetTweets(url).Select(categorizer.Categorize);
+        }
+
+        internal TweetProvider TweetProvider { get; set; }
+    }
+
+    internal class TweetProvider
+    {
+        internal TweetProvider(string[] tweets)
+        {
+            this.tweets = tweets;
+        }
+
+        readonly IEnumerable<string> tweets;
+
+        internal IEnumerable<string> GetTweets(string url)
+        {
+            return tweets ?? GetTweetsFromUrl(url);
+        }
+
+        private IEnumerable<string> GetTweetsFromUrl(string url)
+        {
+            var req = (HttpWebRequest)HttpWebRequest.Create(url);
+            using (var res = req.GetResponse())
+            using (var reader = new StreamReader(res.GetResponseStream()))
+            {
+                 return reader.ReadToEnd().Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+            }
+        }
+    }
+}
