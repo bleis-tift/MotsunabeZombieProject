27190a4284df04a31465ee0e0e31cc2eb4ac7111
bleis-tift <bleis-tift@hotmail.co.jp>
2011-03-21 10:30:45 +0900

    [from now] 2011/03/21 10:30:45 refs #5
    diff --git a/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj b/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj
    index c712d95..788bbb1 100644
    --- a/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj
    +++ b/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj
    @@ -44,6 +44,7 @@
         <Reference Include="System.Xml" />
       </ItemGroup>
       <ItemGroup>
    +    <Compile Include="CategorizedResultTest.cs" />
         <Compile Include="Properties\AssemblyInfo.cs" />
         <Compile Include="TweetCategorizerTest.cs" />
       </ItemGroup>
    diff --git a/MotsunabeZombieProject/MotsunabeZombieProject.csproj b/MotsunabeZombieProject/MotsunabeZombieProject.csproj
    index 03da7f3..e64d79a 100644
    --- a/MotsunabeZombieProject/MotsunabeZombieProject.csproj
    +++ b/MotsunabeZombieProject/MotsunabeZombieProject.csproj
    @@ -40,6 +40,7 @@
         <Reference Include="System.Xml" />
       </ItemGroup>
       <ItemGroup>
    +    <Compile Include="CategorizedResult.cs" />
         <Compile Include="Properties\AssemblyInfo.cs" />
         <Compile Include="TweetCategorizer.cs" />
       </ItemGroup>

---
+++ MotsunabeZombieProject.Tests/CategorizedResultTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MotsunabeZombieProject;
+
+namespace MotsunabeZombieProject.Tests
+{
+    [TestFixture]
+    class CategorizedResultTest
+    {
+        [Test]
+        public void CategorizedResultは元のTweet本体を持っている()
+        {
+            var cr = new CategorizedResult("ほげほげ");
+            Assert.That(cr.Body, Is.EqualTo("ほげほげ"));
+        }
+
+        [Test]
+        public void 判別結果を持っている()
+        {
+            var cr = new CategorizedResult("ほげほげ", "Normal");
+            Assert.That(cr.Categories, Is.EqualTo(new[] { "Normal" }));
+        }
+    }
+}
---
+++ MotsunabeZombieProject/CategorizedResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotsunabeZombieProject
+{
+    public class CategorizedResult
+    {
+        public CategorizedResult(string body, params string[] categories)
+        {
+            Body = body;
+            Categories = categories;
+        }
+
+        public string Body { get; private set; }
+
+        public IEnumerable<string> Categories { get; private set; }
+    }
+}
