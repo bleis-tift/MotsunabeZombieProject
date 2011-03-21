d4d8255c62db24ba333a11aca1690d372a1e4199
bleis-tift <bleis-tift@hotmail.co.jp>
2011-03-21 12:13:24 +0900

    [from now] 2011/03/21 12:13:25 refs #6
    diff --git a/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj b/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj
    index 788bbb1..d7b78a3 100644
    --- a/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj
    +++ b/MotsunabeZombieProject.Tests/MotsunabeZombieProject.Tests.csproj
    @@ -46,6 +46,7 @@
       <ItemGroup>
         <Compile Include="CategorizedResultTest.cs" />
         <Compile Include="Properties\AssemblyInfo.cs" />
    +    <Compile Include="TweetAnalyzerTest.cs" />
         <Compile Include="TweetCategorizerTest.cs" />
       </ItemGroup>
       <ItemGroup>
    diff --git a/MotsunabeZombieProject.Tests/TweetAnalyzerTest.cs b/MotsunabeZombieProject.Tests/TweetAnalyzerTest.cs
    new file mode 100644
    index 0000000..ea76b8c
    --- /dev/null
    +++ b/MotsunabeZombieProject.Tests/TweetAnalyzerTest.cs
    @@ -0,0 +1,20 @@
    +﻿using System;
    +using System.Collections.Generic;
    +using System.Linq;
    +using System.Text;
    +using NUnit.Framework;
    +using MotsunabeZombieProject;
    +
    +namespace MotsunabeZombieProject.Tests
    +{
    +    [TestFixture]
    +    class TweetAnalyzerTest
    +    {
    +        [Test]
    +        public void ネットワークからTweetを取得して解析できる()
    +        {
    +            var ta = new TweetAnalyzer();
    +            Assert.That(ta.Categorize("http://192.168.1.40:4567/public_timeline").Select(r => r.Categories), Is.EqualTo(new[] { "Normal" }));
    +        }
    +    }
    +}
    diff --git a/MotsunabeZombieProject/MotsunabeZombieProject.csproj b/MotsunabeZombieProject/MotsunabeZombieProject.csproj
    index e64d79a..5112233 100644
    --- a/MotsunabeZombieProject/MotsunabeZombieProject.csproj
    +++ b/MotsunabeZombieProject/MotsunabeZombieProject.csproj
    @@ -42,6 +42,7 @@
       <ItemGroup>
         <Compile Include="CategorizedResult.cs" />
         <Compile Include="Properties\AssemblyInfo.cs" />
    +    <Compile Include="TweetAnalyzer.cs" />
         <Compile Include="TweetCategorizer.cs" />
       </ItemGroup>
       <Import Project="$(MSBuildToolsPath)\Microsoft.CSharp.targets" />
    diff --git a/MotsunabeZombieProject/TweetAnalyzer.cs b/MotsunabeZombieProject/TweetAnalyzer.cs
    new file mode 100644
    index 0000000..fc2ca27
    --- /dev/null
    +++ b/MotsunabeZombieProject/TweetAnalyzer.cs
    @@ -0,0 +1,15 @@
    +﻿using System;
    +using System.Collections.Generic;
    +using System.Linq;
    +using System.Text;
    +
    +namespace MotsunabeZombieProject
    +{
    +    public class TweetAnalyzer
    +    {
    +        internal IEnumerable<CategorizedResult> Categorize(string url)
    +        {
    +            throw new NotImplementedException();
    +        }
    +    }
    +}

---
+++ MotsunabeZombieProject.Tests/TweetAnalyzerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MotsunabeZombieProject;
+
+namespace MotsunabeZombieProject.Tests
+{
+    [TestFixture]
+    class TweetAnalyzerTest
+    {
+        [Test]
+        public void ネットワークからTweetを取得して解析できる()
+        {
+            var ta = new TweetAnalyzer();
+            Assert.That(ta.Categorize("http://192.168.1.40:4567/public_timeline").Select(r => r.Categories), Is.EqualTo(new[] { "Normal" }));
+        }
+    }
+}
---
+++ MotsunabeZombieProject/TweetAnalyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MotsunabeZombieProject
+{
+    public class TweetAnalyzer
+    {
+        internal IEnumerable<CategorizedResult> Categorize(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
